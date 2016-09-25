652f8f0ca677326ca9fdf9667803bbeee91a8f64
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-09-25 13:54:29 +0200

    helmet color has added
    Changes to be committed:
    	modified:   helmet/app.js
    	modified:   helmet/index.html
    	modified:   helmet/templates/hello-world.html

--- helmet/app.js
+++ helmet/app.js
@@ -2,6 +2,7 @@
     'use strict';
 
     const APP_NAME = 'Learning AngularJS Directives> Helmet';
+    const HELMET_COLOR = 'red';
     const HTML_URL = './templates/hello-world.html';
     const IMAGE_URL = './images/helmet.svg';
 
@@ -13,16 +14,23 @@
     MainController.$inject = ['$scope'];
     function MainController($scope) {
         $scope.appName = APP_NAME;
+        $scope.helmetColor = HELMET_COLOR;
     }
 
     function HelloWorld() {
         return {
+            scope: {
+                appName: '@applicationName'
+            },
             templateUrl: HTML_URL
         };
     }
 
     function Helmet() {
         return {
+            scope: {
+                helmetColor: '@color'
+            },
             templateUrl: IMAGE_URL
         };
     }
