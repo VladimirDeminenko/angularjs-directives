c55444ccfd735b73c194fb4f9afe67ce00af1a8f
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-09-25 14:34:53 +0200

    Changes to be committed: 	modified:   helmet/app.js 	modified:   helmet/images/helmet.svg

--- helmet/app.js
+++ helmet/app.js
@@ -29,7 +29,7 @@
     function Helmet() {
         return {
             scope: {
-                helmetColor: '@color'
+                color: '@'
             },
             templateUrl: IMAGE_URL
         };
